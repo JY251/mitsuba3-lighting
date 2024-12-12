2244417cfe0978d8f9dce0fb039530a2a5af41aa
xyj <101054474+JY251@users.noreply.github.com>
2024-12-12 20:03:02 +0900

    Partially implemented "computeSunCoordinates"

--- src/emitters/sun.cpp
+++ src/emitters/sun.cpp
@@ -20,6 +20,9 @@ NAMESPACE_BEGIN(mitsuba)
 # define SUN_PIXELFORMAT Bitmap::ESpectrum
 #endif
 
+#define EARTH_MEAN_RADIUS 6371.01   // In km
+#define ASTRONOMICAL_UNIT 149597890 // In km
+
 /*!\plugin{sun}{Sun emitter}
  * \icon{emitter_sun}
  * \order{7}
@@ -89,6 +92,42 @@ NAMESPACE_BEGIN(mitsuba)
 /////////////////////////////////////// Header ///////////////////////////////////////
 // MI_IMPORT_BASE()
 // MI_IMPORT_TYPES(Frame3f, Spectrum)
+
+template <typename Float>
+struct DateTimeRecord {
+    int year;
+    int month;
+    int day;
+    Float hour;
+    Float minute;
+    Float second;
+
+    std::string toString() const {
+        std::ostringstream oss;
+        oss << "DateTimeRecord[year = " << year
+            << ", month= " << month
+            << ", day = " << day
+            << ", hour = " << hour
+            << ", minute = " << minute
+            << ", second = " << second << "]";
+        return oss.str();
+    }
+};
+
+template <typename Float>
+struct LocationRecord {
+    Float longitude;
+    Float latitude;
+    Float timezone;
+
+    std::string toString() const {
+        std::ostringstream oss;
+        oss << "LocationRecord[latitude = " << latitude
+            << ", longitude = " << longitude
+            << ", timezone = " << timezone << "]";
+        return oss.str();
+    }
+};
 
 template <typename Float>
 struct SphericalCoordinates {
@@ -419,30 +458,78 @@ public:
 
         MI_IMPORT_CORE_TYPES() // Frame3f
 
+        ////////////////// Supplementary Constants //////////////////
+        /* The following is from the implementation of "A Practical Analytic Model for
+        Daylight" by A.J. Preetham, Peter Shirley, and Brian Smits */
 
-		SunEmitter(const Properties &props): Base(props) {
-				m_scale = props.get<Float>("scale", 1.0f);
-				m_resolution = props.get<int>("resolution", 512);
-				m_sun = computeSunCoordinates<Float>(props);
-				m_sunRadiusScale = props.get<Float>("sunRadiusScale", 1.0f);
-				m_turbidity = props.get<Float>("turbidity", 3.0f);
-				m_stretch = props.get<Float>("stretch", 1.0f);
+        /* All data lifted from MI. Units are either [] or cm^-1. refer when in doubt MI */
 
-				m_sun = SphericalCoordinates<Float>(props);
-				
-				// The followings are done in "configure" function in mituba1
+        // k_o Spectrum table from pg 127, MI.
+        Float k_oWavelengths[64] = {
+            300, 305, 310, 315, 320, 325, 330, 335, 340, 345,
+            350, 355, 445, 450, 455, 460, 465, 470, 475, 480,
+            485, 490, 495, 500, 505, 510, 515, 520, 525, 530,
+            535, 540, 545, 550, 555, 560, 565, 570, 575, 580,
+            585, 590, 595, 600, 605, 610, 620, 630, 640, 650,
+            660, 670, 680, 690, 700, 710, 720, 730, 740, 750,
+            760, 770, 780, 790
+        };
 
-				SphericalCoordinates<Float> sun(m_sun);
-				sun.elevation *= m_stretch;
-				m_sunDir = toSphere<Float>(sun);
+        Float k_oAmplitudes[65] = {
+            10.0, 4.8, 2.7, 1.35, .8, .380, .160, .075, .04, .019, .007,
+            .0, .003, .003, .004, .006, .008, .009, .012, .014, .017,
+            .021, .025, .03, .035, .04, .045, .048, .057, .063, .07,
+            .075, .08, .085, .095, .103, .110, .12, .122, .12, .118,
+            .115, .12, .125, .130, .12, .105, .09, .079, .067, .057,
+            .048, .036, .028, .023, .018, .014, .011, .010, .009,
+            .007, .004, .0, .0
+        };
 
-                /* Solid angle covered by the sun */
-                m_theta = degToRad<Float>(SUN_APP_RADIUS * 0.5f);
-                m_solidAngle = 2 * M_PI * (1 - dr::cos(m_theta));
-                m_radiance = computeSunRadiance(m_sun.elevation, m_turbidity) * m_scale;
-		}
+        // k_g Spectrum table from pg 130, MI.
+        Float k_gWavelengths[4] = {
+            759, 760, 770, 771
+        };
 
+        Float k_gAmplitudes[4] = {
+            0, 3.0, 0.210, 0
+        };
 
+        // k_wa Spectrum table from pg 130, MI.
+        Float k_waWavelengths[13] = {
+            689, 690, 700, 710, 720,
+            730, 740, 750, 760, 770,
+            780, 790, 800
+        };
+
+        Float k_waAmplitudes[13] = {
+            0, 0.160e-1, 0.240e-1, 0.125e-1,
+            0.100e+1, 0.870, 0.610e-1, 0.100e-2,
+            0.100e-4, 0.100e-4, 0.600e-3,
+            0.175e-1, 0.360e-1
+        };
+
+        /* Wavelengths corresponding to the table below */
+        Float solWavelengths[38] = {
+            380, 390, 400, 410, 420, 430, 440, 450,
+            460, 470, 480, 490, 500, 510, 520, 530,
+            540, 550, 560, 570, 580, 590, 600, 610,
+            620, 630, 640, 650, 660, 670, 680, 690,
+            700, 710, 720, 730, 740, 750
+        };
+
+        /* Solar amplitude in watts / (m^2 * nm * sr) */
+        Float solAmplitudes[38] = {
+            16559.0, 16233.7, 21127.5, 25888.2, 25829.1,
+            24232.3, 26760.5, 29658.3, 30545.4, 30057.5,
+            30663.7, 28830.4, 28712.1, 27825.0, 27100.6,
+            27233.6, 26361.3, 25503.8, 25060.2, 25311.6,
+            25355.9, 25134.2, 24631.5, 24173.2, 23685.3,
+            23212.1, 22827.7, 22339.8, 21970.2, 21526.7,
+            21097.9, 20728.3, 20240.4, 19870.8, 19427.2,
+            19072.4, 18628.9, 18259.2
+        };
+
+        ////////////////// Supplementary Fun //////////////////
         Frame3f toSphere(const SphericalCoordinates<Float> coords) {
                 // converts spherical coordinates to cartesian coordinates
             Float sinTheta, cosTheta, sinPhi, cosPhi;
@@ -508,6 +595,142 @@ public:
 
             return discretized;
         }
+        
+        /**
+        * \brief Compute the elevation and azimuth of the sun as seen by an observer
+        * at \c location at the date and time specified in \c dateTime.
+        *
+        * Based on "Computing the Solar Vector" by Manuel Blanco-Muriel,
+        * Diego C. Alarcon-Padilla, Teodoro Lopez-Moratalla, and Martin Lara-Coira,
+        * in "Solar energy", vol 27, number 5, 2001 by Pergamon Press.
+        */
+        SphericalCoordinates<Float> computeSunCoordinates(const DateTimeRecord<Float> &dateTime, const LocationRecord<Float> &location) {
+            // Main variables
+            double elapsedJulianDays, decHours;
+            double eclipticLongitude, eclipticObliquity;
+            double rightAscension, declination;
+            double elevation, azimuth;
+
+            // Auxiliary variables
+            double dY;
+            double dX;
+
+            /* Calculate difference in days between the current Julian Day
+            and JD 2451545.0, which is noon 1 January 2000 Universal Time */
+            {
+                // Calculate time of the day in UT decimal hours
+                decHours = dateTime.hour - location.timezone +
+                    (dateTime.minute + dateTime.second / 60.0 ) / 60.0;
+                
+                // Calculate current Julian Day
+                int liAux1 = (dateTime.month-14) / 12;
+                int liAux2 = (1461*(dateTime.year + 4800 + liAux1)) / 4
+                    + (367 * (dateTime.month - 2 - 12 * liAux1)) / 12
+                    - (3 * ((dateTime.year + 4900 + liAux1) / 100)) / 4
+                    + dateTime.day - 32075;
+                double dJulianDate = (double) liAux2 - 0.5 + decHours / 24.0;
+
+                // Calculate difference between current Julian Day and JD 2451545.0
+                elapsedJulianDays = dJulianDate - 2451545.0;
+            }
+
+            /* Calculate ecliptic coordinates (ecliptic longitude and obliquity of the
+            ecliptic in radians but without limiting the angle to be less than 2*Pi
+            (i.e., the result may be greater than 2*Pi) */
+            {
+                double omega = 2.1429 - 0.0010394594 * elapsedJulianDays;
+                double meanLongitude = 4.8950630 + 0.017202791698 * elapsedJulianDays; // Radians
+                double anomaly = 6.2400600 + 0.0172019699 * elapsedJulianDays;
+
+                eclipticLongitude = meanLongitude + 0.03341607 * std::sin(anomaly)
+                    + 0.00034894 * std::sin(2*anomaly) - 0.0001134
+                    - 0.0000203 * std::sin(omega);
+
+                eclipticObliquity = 0.4090928 - 6.2140e-9 * elapsedJulianDays
+                    + 0.0000396 * std::cos(omega);
+            }
+
+            // Calculate local coordinates (azimuth and zenith angle) in degrees
+            {
+                double greenwichMeanSiderealTime = 6.6974243242
+                    + 0.0657098283 * elapsedJulianDays + decHours;
+
+                double localMeanSiderealTime = degToRad((Float) ((greenwichMeanSiderealTime * 15
+                    + location.longitude)));
+
+                double latitudeInRadians = degToRad(location.latitude);
+                double cosLatitude = std::cos(latitudeInRadians);
+                double sinLatitude = std::sin(latitudeInRadians);
+
+                double hourAngle = localMeanSiderealTime - rightAscension;
+                double cosHourAngle = std::cos(hourAngle);
+
+                elevation = std::acos(cosLatitude * cosHourAngle
+                    * std::cos(declination) + std::sin(declination) * sinLatitude);
+
+                dY = -std::sin(hourAngle);
+                dX = std::tan(declination) * cosLatitude - sinLatitude * cosHourAngle;
+
+                azimuth = std::atan2(dY, dX);
+                if (azimuth < 0.0)
+                    azimuth += 2*M_PI;
+
+                // Parallax Correction
+                elevation += (EARTH_MEAN_RADIUS / ASTRONOMICAL_UNIT) * std::sin(elevation);
+            }
+
+            return SphericalCoordinates((Float) elevation, (Float) azimuth);
+        }
+
+        SphericalCoordinates computeSunCoordinates(const Vector& sunDir, const Transform &worldToLuminaire) {
+            return fromSphere(normalize(worldToLuminaire(sunDir)));
+        }
+        
+        SphericalCoordinates<Float> computeSunCoordinates(const Properties &props) {
+            /* configure position of sun */
+            if (props.has_property("sunDirection")) {
+                if (props.has_property("latitude") || props.has_property("longitude")
+                    || props.has_property("timezone") || props.has_property("day")
+                    || props.has_property("time"))
+                    // I have replaced "Slog" in mitsuba1 with "Throw", though their purpose might differ a little
+                    Throw("Both the 'sunDirection' parameter and time/location "
+                            "information were provided -- only one of them can be specified at a time!");
+
+                return computeSunCoordinates(
+                    props.getVector("sunDirection"),
+                    props.getAnimatedTransform("toWorld", Transform())->eval(0).inverse());
+            } else {
+            }
+        }
+
+
+
+
+
+        ////////////////// Main Part //////////////////
+		SunEmitter(const Properties &props): Base(props) {
+				m_scale = props.get<Float>("scale", 1.0f);
+				m_resolution = props.get<int>("resolution", 512);
+				m_sun = computeSunCoordinates<Float>(props);
+				m_sunRadiusScale = props.get<Float>("sunRadiusScale", 1.0f);
+				m_turbidity = props.get<Float>("turbidity", 3.0f);
+				m_stretch = props.get<Float>("stretch", 1.0f);
+
+				m_sun = SphericalCoordinates<Float>(props);
+				
+				// The followings are done in "configure" function in mituba1
+
+				SphericalCoordinates<Float> sun(m_sun);
+				sun.elevation *= m_stretch;
+				m_sunDir = toSphere(sun);
+
+                /* Solid angle covered by the sun */
+                m_theta = degToRad<Float>(SUN_APP_RADIUS * 0.5f);
+                m_solidAngle = 2 * M_PI * (1 - dr::cos(m_theta));
+                m_radiance = computeSunRadiance(m_sun.elevation, m_turbidity) * m_scale;
+		}
+
+
 
 protected:
         MI_DECLARE_CLASS()
@@ -528,10 +751,12 @@ protected:
     Frame3f m_sunDir;
     /// Turbidity of the atmosphere
     Float m_turbidity;
+    /// Radiance arriving from the sun disk
+    Spectrum m_radiance;
     /// Stretch factor to extend to the bottom hemisphere
     Float m_stretch;
 };
 
 MI_IMPLEMENT_CLASS_VARIANT(SunEmitter, Emitter)
-MI_EXPORT_PLUGIN(SunEmitter, "Sun emitter");
+MI_EXPORT_PLUGIN(SunEmitter, "Sun emitter")
 NAMESPACE_END(mitsuba)
